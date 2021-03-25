980554c1205de4b7fa9c92b8b5d50de66b63f907
miiunii <dnflwlq3231@naver.com>
2021-03-25 21:27:20 +0900

    type: ADD body: 10강 JPA를 통한 CRUD 배우기 1_2 학습완료

--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity // == table
+@Entity(name = "user") // == table
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -4,10 +4,13 @@ import com.example.study.StudyApplication;
 import com.example.study.model.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
-public class UserRepositoryTest extends StudyApplication {
+@SpringBootTest
+public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
@@ -15,22 +18,41 @@ public class UserRepositoryTest extends StudyApplication {
     @Test
     public void create() {
         User user = new User();
-        user.setAccount("TestUser01");
+        user.setAccount("TestUser02");
         user.setEmail("TestUser@gmail.com");
-        user.setPhoneNumber("010-1111-1111");
+        user.setPhoneNumber("010-2222-2222");
         user.setCreatedAt(LocalDateTime.now());
-        user.setCreatedBy("admin");
+        user.setCreatedBy("test2");
 
         User newUser = userRepository.save(user);
         System.out.println("newUser : " + newUser);
         System.out.println(user);
     }
+
+    @Test
     public void read() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            System.out.println("user : " + selectUser);
+        });
 
     }
+
+    @Test
     public void update() {
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("pppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
     }
+
+    @Test
     public void delete() {
 
     }
