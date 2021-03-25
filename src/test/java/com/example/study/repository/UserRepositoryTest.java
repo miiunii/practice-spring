7253c1a0b8fe6409be2cec8da69d7a0dc73d4345
miiunii <dnflwlq3231@naver.com>
2021-03-25 20:59:57 +0900

    type: ADD body: 9강 JPA를 통한 CRUD 배우기 1_1 학습완료

---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplication;
+import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class UserRepositoryTest extends StudyApplication {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create() {
+        User user = new User();
+        user.setAccount("TestUser01");
+        user.setEmail("TestUser@gmail.com");
+        user.setPhoneNumber("010-1111-1111");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("admin");
+
+        User newUser = userRepository.save(user);
+        System.out.println("newUser : " + newUser);
+        System.out.println(user);
+    }
+    public void read() {
+
+    }
+    public void update() {
+
+    }
+    public void delete() {
+
+    }
+}
