c505b4115b6e803192098a74d7f2fd4a1839b7ff
miiunii <dnflwlq3231@naver.com>
2021-03-25 20:12:26 +0900

    type: ADD body: 7강 JPA 학습완료

---
+++ src/main/java/com/example/study/controller/GetController.java
@@ -0,0 +1,28 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api") //localhost:8080/api
+public class GetController {
+
+    @RequestMapping(method = RequestMethod.GET, path="/getMethod") // localhost:8080/api/getMethod
+    public  String getRequest() {
+        return "Hi get Method";
+    }
+
+    @GetMapping("/getParameter") // localhost:8080/getParameter
+    public String getParameter(@RequestParam String id, @RequestParam String password) {
+        return id+password;
+    }
+
+    @GetMapping("/getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam searchParam) {
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+        return searchParam;
+    }
+}
---
+++ src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,9 @@
+package com.example.study.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User {
+}
