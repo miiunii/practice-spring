5dc6107e8ab063d7a5ca23e75f91c1870191d561
miiunii <dnflwlq3231@naver.com>
2021-03-27 01:21:53 +0900

    type: ADD body: 18장 Entity 및 Repository 설정 -1 완료

--- src/main/java/com/example/study/model/entity/OrderDetail.java
+++ src/main/java/com/example/study/model/entity/OrderDetail.java
@@ -6,20 +6,24 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@ToString(exclude = {"userId", "itemId"})
 public class OrderDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private LocalDateTime orderAt;
-    @ManyToOne
-    private User userId;
-    @ManyToOne
-    private Item itemId;
+    private String status;
+    private LocalDateTime arrivalDate;
+    private Integer quantity;
+    private BigDecimal totalPrice;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+
 }
--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -16,10 +16,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String account;
+    private String password;
+    private String status;
     private String email;
     private String phoneNumber;
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
     private LocalDateTime createdAt;
     private String createdBy;
     private LocalDateTime updatedAt;
